a29cb020e932e76a9e75e11a0942b3bc79fdefc1
erzhena-d <dyrkheevaerzhena@gmail.com>
2025-08-31 18:53:07 +0000

    terraform fmt

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -20,13 +20,13 @@ resource "aws_key_pair" "deployer" {
 }
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  key_name = aws_key_pair.deployer.key_name
-  instance_type = var.instance_type
-  subnet_id = aws_subnet.main.id
+  ami                    = data.aws_ami.ubuntu.id
+  key_name               = aws_key_pair.deployer.key_name
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.main.id
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
 }
 
-output ec2 {
-    value = aws_instance.web.public_ip
+output "ec2" {
+  value = aws_instance.web.public_ip
 }
--- terraform/sg.tf
+++ terraform/sg.tf
@@ -1,43 +1,43 @@
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
-  vpc_id = aws_vpc.main.id
+  vpc_id      = aws_vpc.main.id
 
   ingress {
-    description      = "TLS from VPC"
-    from_port        = 9000
-    to_port          = 9000
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"] # Allow from anywhere, adjust as needed
+    description = "TLS from VPC"
+    from_port   = 9000
+    to_port     = 9000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow from anywhere, adjust as needed
   }
 
   ingress {
-    description      = "TLS from VPC"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"] # Allow from anywhere, adjust as needed
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow from anywhere, adjust as needed
   }
 
-   ingress {
-    description      = "TLS from VPC"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"] # Allow from anywhere, adjust as needed
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow from anywhere, adjust as needed
   }
-    ingress {
-    description      = "TLS from VPC"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
