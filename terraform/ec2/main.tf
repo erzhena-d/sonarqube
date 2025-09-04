392786946ee5f807c150ce6d22bf04f267b32260
erzhena-d <dyrkheevaerzhena@gmail.com>
2025-09-04 22:43:32 +0000

    changes

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -8,14 +8,14 @@ data "aws_ami" "sonarqube" {
 #   }
 }
 
-resource "aws_key_pair" "deployer" {
-  key_name   = "ubuntu-key"
-  public_key = file("/home/ubuntu/.ssh/id_rsa.pub")
-}
+# resource "aws_key_pair" "deployer" {
+#   key_name   = "ubuntu-key"
+#   public_key = file("~/.ssh/id_rsa.pub")
+# }
 
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.sonarqube.id #here packer will provide the AMI ID for the created image
-  key_name               = aws_key_pair.deployer.key_name
+  key_name               = "ubuntu-key"
   instance_type          = var.instance_type
   subnet_id              = var.subnet_id
   vpc_security_group_ids = [var.security_group_id]
